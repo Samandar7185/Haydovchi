7812b011d5fe30726356237d871a2fef4755975f
Samandar7185 <134359911+Samandar7185@users.noreply.github.com>
2026-06-02 09:40:08 +0500

    Configure plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
-full content
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Haydovchi"
+include(":app")
