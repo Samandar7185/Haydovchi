c435ee4c81935b5d2f370d913262abf3df778eca
Samandar7185 <134359911+Samandar7185@users.noreply.github.com>
2026-06-02 09:20:52 +0500

    Add UiState sealed class for better state management

---
+++ app/src/main/java/uz/haydovchi/app/util/UiState.kt
@@ -0,0 +1,7 @@
+package uz.haydovchi.app.util
+
+sealed class UiState<out T> {
+    data object Loading : UiState<Nothing>()
+    data class Success<out T>(val data: T) : UiState<T>()
+    data class Error(val message: String, val throwable: Throwable? = null) : UiState<Nothing>()
+}
